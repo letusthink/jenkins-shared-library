3b8c48055cd316532be19aa1341eb2e29b795e12
letusthink <138984825+letusthink@users.noreply.github.com>
2024-08-26 10:46:13 +0800

    Update checkout.groovy

--- src/org/checkout.groovy
+++ src/org/checkout.groovy
@@ -2,6 +2,6 @@ package org
 
 def scm(build_branch,url){
     checkout scmGit(branches: [[name: "${build_branch}"]], extensions: [], 
-    userRemoteConfigs: [[credentialsId: 'gitlab', url: "${url}"]])        
+    userRemoteConfigs: [[credentialsId: 'GitHub_credentials', url: "${url}"]])        
 }
 
